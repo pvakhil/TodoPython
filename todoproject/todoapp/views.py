8bcdec59ddf5802aabc47badf9d017efb461c0e4
pvakhil <133202583+pvakhil@users.noreply.github.com>
2023-07-26 06:26:19 +0530

    added

---
+++ todoproject/todoapp/views.py
@@ -0,0 +1,18 @@
+from  .models import Task
+
+# Create your views here.
+def add(request):
+    task1= Task.objects.all()
+    if request.method=='POST':
+        name=request.POST.get('task','')
+        priority=request.POST.get('priority','')
+        task=Task(name=name,priority=priority)
+        task=save()
+
+
+    return render(request,'home.html',{'task':task})
+
+# def details(request):
+#
+#     return render(request,'details.html',)
+
